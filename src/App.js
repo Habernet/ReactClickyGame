d295c068432d2781d6db047730b168a7def74317
habernet <phaberern@gmail.com>
2019-07-25 14:24:26 -0400

    Added layout of main app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./App.css";
+// Import all of your components
 
-function App() {
-  return <h1>HELLO WORLD!</h1>;
+//Define a function that will shuffle the cards using random
+
+class App extends Component {
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: []
+  };
+
+  // handle click function checks to see if this is clicked, if it is not, increments and sets state to clicked...else handle reset
+
+  //handle increment function gets the new score via the currentScore..sets state to new score
+  // Test to see if the new score actually was updated
+  // test to see if the max score was reached, set state rightWrong to you win yayy
+
+  // Handle reset function
+  // resets the whole state of the app and shuffles
+
+  //handleshuffle
+  // shuffle the array of friends and then use that to set the new state
+
+  render() {
+    return (
+      
+      // wrapper
+      // nav
+      // container
+      // row
+      // map through friends => 
+      //      col / friend card
+    );
+  }
 }
 
 export default App;
