d706265675deb9174b6b94314c962a71e11bbd07
habernet <phaberern@gmail.com>
2019-07-26 15:23:11 -0400

    Updates

---
+++ precache-manifest.1fdd1acb6567c35d282333bb26e92fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf497ebd8542d7e516c534b7c267bfb",
+    "url": "/ReactClickyGame/index.html"
+  },
+  {
+    "revision": "1ee3d1435fea9fc64b44",
+    "url": "/ReactClickyGame/static/css/main.ea8191a5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ReactClickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1ee3d1435fea9fc64b44",
+    "url": "/ReactClickyGame/static/js/main.de0c7154.chunk.js"
+  },
+  {
+    "revision": "62a959f68693829acc63",
+    "url": "/ReactClickyGame/static/js/runtime~main.8ee770dc.js"
+  },
+  {
+    "revision": "7e98bfcef67ba7635d02ca100e592336",
+    "url": "/ReactClickyGame/static/media/PocketMon.7e98bfce.otf"
+  }
+]);
